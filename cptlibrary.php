f8b4dd321ee662e36d65ca2a39d8c03c6b33be40
mlem47 <malte.lemanczik@outlook.de>
2021-02-24 14:52:30 +0100

    author

--- cptlibrary.php
+++ cptlibrary.php
@@ -13,11 +13,11 @@
  * @package           Plugin_Name
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin Name:       Fachbuchausleihe
  * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Verleih und Verwaltung von Fachbüchern
  * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Author:            Malte Lemanczik
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
