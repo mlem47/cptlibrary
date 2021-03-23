f62f0b5ad3e73d1bc355b095a91569d411921dd1
mlem47 <malte.lemanczik@outlook.de>
2021-03-23 14:08:34 +0100

    insert_post cpt_auftrag

--- public/templates/single-cpt_books.php
+++ public/templates/single-cpt_books.php
@@ -1,4 +1,108 @@
-<?php get_header(); ?>
+<?php get_header();?>
+
+
+<form id="post_entry" name="post_entry" method="post" action="">
+    <p>
+        <label>Auftragstitel</label><br />
+        <input type="text" id="post_title" name="post_title" value="<?php the_title() ?>"/>
+	</p>
+	
+    <p>
+        <label>Name & E-Mail</label><br />
+		<input type="text" id="post_desc" name="post_desc" />
+	</p>
+	
+    <p>
+	<div class="form-group">
+					<label for="post_einrichtungSelect">Einrichtung</label>
+					<select class="form-control" id="einrichtungSelect" name="post_einrichtungSelect">
+					<option selected="selected">Einrichtung auswählen</option>
+
+					<?php 
+					$items = get_posts( array(
+						'post_type'      => 'cpt_einrichtung',
+						'post_status'    => 'publish',
+						'order' 	 => 'ASC',
+						'posts_per_page' => -1
+						) );
+					if ( $items) {
+							foreach ( $items as $item ) {
+								$item_name =  $item->post_title;
+								echo '<option >'.$item_name.'</option>';
+							}
+					}
+
+					?>
+					</select>
+		</div>
+        <label>Einrichtung</label><br />
+        <input type="text" id="post_artist" name="post_artist" />
+        <input type="hidden" name="post_type" id="post_type" value="cpt_auftrag" />
+        <input type="hidden" id="post_action" name="post_action" value="post" />
+    </p>
+    <p>
+        <input type="submit" name="post_submit" value="Submit" />
+    </p>
+    <?php wp_nonce_field( 'new_song_nonce' ); ?>
+</form>
+
+<?php 
+if($_POST['post_submit'] == 'Submit') {
+    $new_auftrag = array(
+        'post_title' => $_POST['post_title'],
+		'post_content' => $_POST['post_desc'],
+		'tax_input' => $taxonomy,
+        'post_type' => 'cpt_auftrag',
+        'post_status' => 'publish',
+        'comment_status' => 'closed',
+        'ping_status' => 'closed'
+	);
+	
+	$taxonomy = array(
+		'color' => array(
+			'green',
+			'red'
+		),
+	);
+
+//// INSERT TERM ////https://tecserve.eu/wordpress/tax_input-in-wp_insert_post-richtig-verwenden/ /////
+
+	$department = 'Food';
+	$section = 'Produce';
+	// Check if department exists
+	$department_term = term_exists( $department, 'category', 0 );
+	// Create if not
+	if ( !$department_term ) {
+		$department_term = wp_insert_term( $department, 'category', array( 'parent' => 0) );
+	}
+	// Check if section exists as child of department
+	$section_term = term_exists( $section, 'category', $department_term['term_taxonomy_id'] );
+	// Create if not
+	if ( !$section_term ) {
+		$section_term = wp_insert_term( $section, 'category', array( 'parent' => $department_term['term_taxonomy_id'] ) );
+	}
+	$taxonomy = array(
+		'category' => array(
+			$section_term['term_taxonomy_id'],
+			$department_term['term_taxonomy_id']
+		)
+	);
+	$post = array(
+		'post_title' => 'Hallo neuer Beitrag!',
+		'post_content' => 'Hier steht ganz viel Inhalt',
+		'tax_input' => $taxonomy
+	);
+	$post_id = wp_insert_post( $post );
+
+    $post_id = wp_insert_post($new_auftrag);
+    add_post_meta($post_id, "_song_artist", $_POST['post_artist']);
+}
+?>
+
+
+
+
+
 
  <div class="container-singlecptbooks">
        
@@ -31,6 +135,9 @@
 
 
 	<?php endwhile; endif; ?>
+
+
+
 
 
 	<div class="row">
@@ -81,20 +188,24 @@
 
 			<?php
 			// Create post object
-			$customtax =  array(
-				'Kennziffer' => $kennziffer,
-			 );
-			$cpt_auftrag = array(
+			
+			$new_auftrag = array(
 			'post_title'    => wp_strip_all_tags( $_POST['$title'] ),
 			'post_status'   => 'publish',
 			'post_date'		=> '',
 			'post_author'   => 1,
 			'post_type'		=> 'cpt_auftrag',
-			'tax_input' => $customtax
+			'tax_input' 	=> 'Kennziffer'
 			);
 			
 			// Insert the post into the database
-			wp_insert_post( $cpt_auftrag );
+			$post_id = wp_insert_post( $new_auftrag );
+
+			if($post_id){
+				echo "Post successfully published!";
+			} else {
+				echo "Something went wrong, try again.";
+			}
 
 			?>
 
